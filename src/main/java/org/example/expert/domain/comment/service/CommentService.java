103290011d0ac37df9153479debaac9542c49d73
Sungju Kim <sjkim20010909@gmail.com>
2024-10-08 20:48:15 +0900

    1-7 Join fetch

--- src/main/java/org/example/expert/domain/comment/service/CommentService.java
+++ src/main/java/org/example/expert/domain/comment/service/CommentService.java
@@ -50,6 +50,7 @@ public class CommentService {
     public List<CommentResponse> getComments(long todoId) {
         List<Comment> commentList = commentRepository.findByTodoIdWithUser(todoId);
 
+
         List<CommentResponse> dtoList = new ArrayList<>();
         for (Comment comment : commentList) {
             User user = comment.getUser();
